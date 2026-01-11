696f598df442e1b6fa29d305764d7408adb2cc80
nsandovala <nandres.sandoval@gmail.com>
2026-01-10 21:32:33 -0300

    fix(shop): export default and force port 4200

--- src/app/layouts/public-layout.component.ts
+++ src/app/layouts/public-layout.component.ts
@@ -6,9 +6,7 @@ import { RouterOutlet } from '@angular/router';
     standalone: true,
     imports: [RouterOutlet],
     template: `
-    <div class="min-h-screen bg-[#121212]">
-        <router-outlet></router-outlet>
-    </div>
+    <div class="min-h-screen bg-white text-black">Layout loaded <router-outlet></router-outlet></div>
   `
 })
 export class PublicLayoutComponent { 
